df08b6c62ffc76cfec677b4f52e47263a3af1e9c
Alandin WZ <alanwatahyde@gmail.com>
2026-05-20 22:07:40 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,8 @@
+const numeros = [14, 16, 20, 42];
+
+const somaTotal = numeros.reduce((acumulador, atual) => acumulador + atual, 0);
+
+
+const somaTotal = numeros.reduce((acumulador, atual) => acumulador + atual, 0);
+
+console.log(somaTotal); 
